0a28d2c02fa85cf23213abd8b04601103eca7471
Omar <ohawary98@gmail.com>
2023-11-21 17:05:18 +0200

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appbar: AppBar(),
+        appBar: AppBar(),
         body: Center(
           child: Text('Trying merge'),
         ),
