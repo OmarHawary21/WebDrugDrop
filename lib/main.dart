796d937631665ae3b7847c0224f5cf1b44af4898
Omar <ohawary98@gmail.com>
2023-11-21 17:12:35 +0200

    final test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar()
+        appBar: AppBar(),
         body: Center(
           child: Text('Push Test'),
         ),
