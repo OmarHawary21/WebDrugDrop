012e5bb887ffa5301ba8277495b6b279608a73f6
Omar <ohawary98@gmail.com>
2023-11-22 14:37:44 +0200

    Log In finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/log_in_screen.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,12 +12,34 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          child: Text('Pushed Test by Omar'),
+      debugShowCheckedModeBanner: false,
+      title: 'DrugDrop',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(255, 252, 252, 1),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromRGBO(3, 37, 78, 1),
+          secondary: const Color.fromRGBO(219, 243, 250, 1),
+        ),
+        appBarTheme: AppBarTheme(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+            ),
+          ),
+          toolbarHeight: MediaQuery.of(context).size.height * 0.08,
+          color: const Color.fromRGBO(230, 240, 255, 1),
+          titleTextStyle: const TextStyle(
+            color: Color.fromRGBO(3, 37, 78, 1),
+            fontSize: 20,
+            fontFamily: 'PollerOne',
+          ),
         ),
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (_) => LogInScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/log_in_screen.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+import '../../widgets/logo.dart';
+import '../../widgets/olives.dart';
+
+class LogInScreen extends StatefulWidget {
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+var _isVisible;
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  void initState() {
+    _isVisible = true;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var media = MediaQuery.of(context);
+
+    return Scaffold(
+      body: FadeInUp(
+        duration: const Duration(milliseconds: 2000),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Logo(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: media.size.height * 0.7,
+                        child: Image.asset('assets/images/Login.png'),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FittedBox(
+                        child: Text(
+                          'Login',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'PollerOne',
+                            fontSize: 20,
+                            color: theme.colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                      FittedBox(
+                        child: Text(
+                          'sign in to continue',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 15,
+                            color: theme.colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                      Forms(),
+                      SizedBox(height: media.size.height * 0.025,),
+                      Buttons(),
+                    ],
+                  ),
+                ],
+              ),
+              Olives(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Forms extends StatefulWidget {
+  @override
+  State<Forms> createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var media = MediaQuery.of(context);
+    return Form(
+      child: Column(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            elevation: 10,
+            color: const Color.fromRGBO(230, 240, 255, 1),
+            child: TextFormField(
+              maxLength: 8,
+              maxLines: 1,
+              textAlignVertical: TextAlignVertical.center,
+              textInputAction: TextInputAction.next,
+              keyboardType: TextInputType.phone,
+              style: TextStyle(color: theme.colorScheme.primary),
+              decoration: InputDecoration(
+                constraints: BoxConstraints(
+                  maxHeight: media.size.height * 0.055,
+                  maxWidth: media.size.width * 0.3,
+                ),
+                isDense: true,
+                counterText: '',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                label: const Text('Phone Number'),
+                prefixIcon: Icon(
+                  Icons.phone,
+                  color: theme.colorScheme.primary,
+                  size: 18,
+                ),
+                prefix: Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Text(
+                    '09',
+                    style: TextStyle(color: theme.colorScheme.primary),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: media.size.height * 0.015,
+          ),
+          Card(
+            color: const Color.fromRGBO(230, 240, 255, 1),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: TextFormField(
+              obscureText: _isVisible,
+              textInputAction: TextInputAction.done,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: InputDecoration(
+                constraints: BoxConstraints(
+                  maxHeight: media.size.height * 0.055,
+                  maxWidth: media.size.width * 0.3,
+                ),
+                isDense: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                label: const Text('Password'),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: theme.colorScheme.primary,
+                  size: 18,
+                ),
+                suffixIcon: IconButton(
+                  splashRadius: 20,
+                  onPressed: () => setState(() => _isVisible = !_isVisible),
+                  icon: Icon(
+                    _isVisible ? Icons.visibility : Icons.visibility_off,
+                    color: theme.colorScheme.primary,
+                    size: 18,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Buttons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+          width: MediaQuery.of(context).size.width * 0.2,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            child: const Text(
+              'Login',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            onPressed: () {},
+            child: const FittedBox(
+              child: Text(
+                'Forgot your password?',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const FittedBox(
+                child: Text('Don\'t have an account? '),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+                child: const FittedBox(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
